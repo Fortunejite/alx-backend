a9f4385c747151318d4f2808444b9d72c8440cd1
Fortunejite <fortunejite1234@gmail.com>
2023-07-26 21:25:08 +0100

    update

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,23 +13,37 @@ class LRUCache(BaseCaching):
         """ Initiliaze
         """
         super().__init__()
-        self.last = ''
+        self.least_recently_used = ''
+
+    def get_least_recently_used(self):
+        least = next(iter(self.cache_data))
+        for key in self.cache_data:
+            if self.cache_data[key][0] > self.cache_data[least][0]:
+                least = key
+        return least
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """ Add an item in the cache using LRU
         """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 if key not in self.cache_data:
-                    print("DISCARD: {}".format(self.last))
-                    del self.cache_data[self.last]
-            self.cache_data[key] = item
-            self.last = key
+                    least = self.get_least_recently_used()
+                    print("DISCARD: {}".format(least))
+                    del self.cache_data[least]
+            self.cache_data[key] = [1, item]
 
     def get(self, key):
         """ Get an item by key
         """
         if key is not None and key in self.cache_data:
-            self.last = key
-            return self.cache_data[key]
+            self.cache_data[key][0] += 1
+            return self.cache_data[key][1]
         return None
+    
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)[1]))
