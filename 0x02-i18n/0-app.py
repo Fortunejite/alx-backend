d2845c5b938c63309bdfeb1cfbd4c35321bb8b1c
Fortunejite <fortunejite1234@gmail.com>
2023-08-01 19:38:51 +0100

    Update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-	return render_template('0-index.html')
 
-if __name__ == "__main__":
-	app.run()
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
